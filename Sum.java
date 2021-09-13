222c2d1f5d5fca8aa3128ed39277854af6c20439
azhan14 <azhanpatel77@gmail.com>
2021-09-13 12:23:14 +0530

    Message Change in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
 	public static void main(String[] args){
 		int num1 = 5, num2 = 10, sum;
 		sum = num1 + num2;
-		System.out.println("Sum: "+sum);
+		System.out.println("Sum of numbers is: "+sum);
 	}
 }
